e6d71323fafe8bf8c3dc423c741f588ee29ad851
Mustafayev19 <azemustafayev19@gmail.com>
2025-06-11 23:37:42 +0400

    .

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Render və digər hostlar üçün CORS açıq olmalıdır
+  app.enableCors({
+    origin: '*', // təhlükəsizliyə görə sonradan bura konkret URL qoymaq olar
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 bootstrap();
