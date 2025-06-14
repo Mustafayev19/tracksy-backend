d0f506545d0a10307cbd0c01cc06b3f791d994ff
Mustafayev19 <azemustafayev19@gmail.com>
2025-06-14 19:22:25 +0400

    .

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as express from 'express'; // Express import edildi
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
