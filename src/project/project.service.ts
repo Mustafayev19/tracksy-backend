03b29179dffe16195921ce33ac61c16541b56bc3
Mustafayev19 <azemustafayev19@gmail.com>
2025-06-16 04:25:48 +0400

    .

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
-import { Project } from 'generated/prisma/client';
+import { Project } from '@prisma/client';
 
 @Injectable()
 export class ProjectService {
